50ee903610b9770277a2fed646bd8d07cfc68d70
bitfurry <sahilkharb@iitj.ac.in>
2015-09-04 12:08:41 +0530

    Friday 4th Sept Before lunch

--- ex-1.11-15.rkt
+++ ex-1.11-15.rkt
@@ -26,3 +26,16 @@
         ((= col row) 1)
         (else (+ (pascal (- row 1) (- col 1))
                  (pascal (- row 1) col)))))
+
+;; Solution 1.15
+
+(define (cube x) ( * x x x ))
+
+(define (p x)
+  (print "1\n")
+  (- (* 3 x) (* 4 (cube x))))
+
+(define (sine angle)
+  (if (not (> (abs angle) 0.1))
+      angle
+      (p (sine (/ angle 3.0)))))
