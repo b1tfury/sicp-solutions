61e02138f8c96a4eee1c2a6371e61eb01fa45eb9
bitfurry <sahilkharb@iitj.ac.in>
2015-09-07 14:47:39 +0530

    1.29 -

--- ex-1.21-28.rkt
+++ ex-1.21-28.rkt
@@ -98,3 +98,65 @@
 (search-for-primes-new 10000 10039)
 (search-for-primes-new 100000 100044)
 (search-for-primes-new 1000000 1000038)
+
+
+;;Solution 1.26
+;; The time taken here is in O(n), because mulutplication operator is called after recursive application of expmod
+
+
+;; Solution 1.27
+(define (carmichael? n)
+  (define (expmod base exp m)
+    (cond ((= exp 0) 1)
+          ((even? exp)
+           (remainder (square (expmod base (/ exp 2) m))
+                      m))
+          (else
+           (remainder (* base (expmod base (- exp 1) m))
+                      m))))
+  (define (fermat-test n a)
+    (= (expmod a n n) a))
+  (define (iter n i)
+    (cond ((= i n) true)
+          ((fermat-test n i) (iter n (+ i 1)))
+          (else false)))
+  (iter n 2))
+
+(newline)
+(newline)
+(display "Solutin 1.27")
+(newline)
+
+(carmichael? 1000)
+(carmichael? 10000)
+(carmichael? 1000000)
+
+
+;; Solution 1.28
+(define (expmod-new base exp m)
+  (define (nontrivial-test x n)
+    (if (and (not (or (= x 1)
+                      (= x (- n 1))))
+         (= (remainder (square x)
+                       n)
+            1))
+        0
+        x))
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (nontrivial-test (expmod-new base (/ exp 2) m) m))
+                    m))
+        (else
+         (remainder (* base (expmod-new base (- exp 1) m))
+                    m))))
+
+(define (miller-rabin-test n)
+  (define (try-it a)
+    (= (expmod a (- n 1) n) 1))
+  (define (iter a)
+    (cond ((= a 0)
+           #t)
+          ((try-it (+ 1 (random (- n 1))))
+           (iter (- a 1)))
+          (else #f)))
+  (iter 10))
---
+++ sec-1.3.1-2.rkt
@@ -0,0 +1,45 @@
+#lang scheme
+
+;; Procedurea as Arguments
+
+(define (cube a) (* a a a))
+
+(define (sum-integers a b)
+  (if (> a b)
+      0
+      ( + a (sum-integers (+ a 1) b))))
+
+(define (sum-cubes a b)
+  (if (> a b)
+      0
+      (+ (cube a) (sum-cubes (+ a 1) b))))
+
+(define (pi-sum a b)
+  (if (> a b)
+      0
+      (+ (/ 1.0 (* a (+ a 2))) (pi-sum (+ a 4) b))))
+
+(define (sum term a next b)
+  (if (> a b) 0
+      ( + (term a)
+          (sum term (next a) next b))))
+(define (inc a ) (+ a 1))
+
+(define (sum-cubes-new a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+
+(define (sum-ints a b)
+  (sum identity a inc b))
+(define (pi-term x)
+  (/ 1.0 (* x (+ x 2))))
+(define (pi-next x)
+  (+ x 4))
+
+(define (pi-sum-new a b)
+  (sum pi-term a pi-next b))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b) dx))
