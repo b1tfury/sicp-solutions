2c47f1e769b5c12dbc6b57b9e3066b08f000fe4a
bitfurry <sahilkharb@iitj.ac.in>
2015-09-09 13:42:26 +0530

    Mapping voer trees

--- chapter2/sec-2.2.rkt
+++ chapter2/sec-2.2.rkt
@@ -81,3 +81,30 @@
 (length x)
 
 (count-leaves x)
+
+;; Mapping over trees
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) null)
+        ((not (pair? tree))
+         (* tree factor))
+        (else
+         (cons (scale-tree (car tree)
+                           factor)
+               (scale-tree (cdr tree)
+                           factor)))))
+
+(define (scale-tree-new tree factor)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (scale-tree-new sub-tree factor)
+             (* sub-tree factor)))
+       tree))
+(scale-tree (list 1
+                  (list 2 (list 3 4) 5)
+                  (list 6 7))
+            10)
+(scale-tree-new (list 1
+                  (list 2 (list 3 4) 5)
+                  (list 6 7))
+            10)
