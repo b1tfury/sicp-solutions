fb348d71ed2f0e62745232f3f7f23d16f676e2b4
bitfurry <sahilkharb@iitj.ac.in>
2015-09-10 12:57:46 +0530

    Sets as trees

---
+++ chapter2/ex-2.63-65.rkt
@@ -0,0 +1,105 @@
+#lang scheme
+
+;; Solution 2.63
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set-tree? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set-tree?
+          x
+          (left-branch set)))
+        ((> x (entry set))
+         (element-of-set-tree?
+          x
+          (right-branch set)))))
+
+(define (adjoin-set-tree x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree
+          (entry set)
+          (adjoin-set-tree x (left-branch set))
+          (right-branch set)))
+        ((> x (entry set))
+         (make-tree
+          (entry set)
+          (left-branch set)
+          (adjoin-set-tree x (right-branch set))))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append
+       (tree->list-1
+        (left-branch tree))
+       (cons (entry tree)
+             (tree->list-1
+              (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list
+         (left-branch tree)
+         (cons (entry tree)
+               (copy-to-list
+                (right-branch tree)
+                result-list)))))
+  (copy-to-list tree '()))
+
+(define t1 '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+
+(tree->list-1 t1)
+(tree->list-2 t1)
+;; The results of both procedures are same
+;; but their order of growth is different
+
+
+;; Solution 2.64
+(define (list->tree elements)
+  (car (partial-tree
+        elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size
+             (quotient (- n 1) 2)))
+        (let ((left-result
+               (partial-tree
+                elts left-size)))
+          (let ((left-tree
+                 (car left-result))
+                (non-left-elts
+                 (cdr left-result))
+                (right-size
+                 (- n (+ left-size 1))))
+            (let ((this-entry
+                   (car non-left-elts))
+                  (right-result
+                   (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+              (let ((right-tree
+                     (car right-result))
+                    (remaining-elts
+                     (cdr right-result)))
+                (cons (make-tree this-entry
+                                 left-tree
+                                 right-tree)
+                      remaining-elts))))))))
+
+
+(list->tree (list 1 3 5 7 9 11))
--- chapter2/sec-2.3.rkt
+++ chapter2/sec-2.3.rkt
@@ -115,3 +115,69 @@
          (union-set (cdr set1) (cons (car set1) set2)))
         (else
          (union-set (cdr set1) set2))))
+
+
+;; Sets as Ordered lists
+
+(define (element-of-set-new? x set)
+  (cond ((null? set)
+         false)
+        ((= x (car set) true))
+        ((< x (car set) false))
+        (else (element-of-set-new? x (cdr set)))))
+
+(define (intersection-set-new set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1)) (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set-new
+                         (cdr set1)
+                         (cdr set2))))
+              ((< x1 x2) (intersection-set-new
+                          (cdr set1)
+                          set2))
+              ((< x2 x1) (intersection-set-new
+                          set1
+                          (cdr set2)))))))
+
+
+;; Sets as binary tree
+
+(define (entry tree) (car tree))
+
+(define (left-branch tree) (cadr tree))
+
+(define (right-branch tree) (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-of-set-tree? x set)
+  (cond ((null? set) false)
+        ((= x (entry set)) true)
+        ((< x (entry set))
+         (element-of-set-tree?
+          x
+          (left-branch set)))
+        ((> x (entry set))
+         (element-of-set-tree?
+          x
+          (right-branch set)))))
+
+(define (adjoin-set-tree x set)
+  (cond ((null? set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree
+          (entry set)
+          (adjoin-set-tree x (left-branch set))
+          (right-branch set)))
+        ((> x (entry set))
+         (make-tree
+          (entry set)
+          (left-branch set)
+          (adjoin-set-tree x (right-branch set))))))
+
+
+;;
