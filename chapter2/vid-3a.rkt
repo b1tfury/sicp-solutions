d9463003ab481d64e8e29f9b1e00c53a18e86f06
bitfurry <sahilkharb@iitj.ac.in>
2015-09-11 12:15:56 +0530

    Lecture 3A - Henderson Escher Example

---
+++ chapter2/vid-3a.rkt
@@ -0,0 +1,35 @@
+#lang scheme
+
+(define (scale-list s l)
+  (if (null? l)
+      null
+      (cons (* (car l) s)
+            (scale-list s (cdr l)))))
+
+(scale-list 10 (list 1 2 3 4 5))
+
+(define (map proc l)
+  (if (null? l)
+      null
+      (cons (proc (car l))
+            (map proc (cdr l)))))
+
+(define (map-new proc l)
+  (define (iter proc l res)
+    (if (null? l)
+        res
+        ((iter
+          proc
+          (cdr l)
+          ((cons res (proc (car l))))))))
+  (iter proc l null))
+
+(define (scale-list-new s l)
+  (map (lambda (x) (* x s)) l))
+
+(scale-list-new 10 (list 1 2 3 4 5))
+
+(define (square x) (* x x))
+(map square (list 1 2 3 4 5))
+
+(map-new square (list 1 2 3 4))
